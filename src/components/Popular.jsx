e77ef0b6f12cfd95338df835dcf7df7b8941e27a
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-05-11 20:01:23 +0700

    map through popular useState

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -11,12 +11,19 @@ function Popular() {
     const getPopular = async () => {
         const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
         const data = await api.json();
-        
-    }
+        setPopular(data.recipes)
+    };
 
-  return (
-    <div>Popular</div>
-  )
+  return  <div>
+      {popular.map((recipe) => {
+        return(
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+        )
+      })}
+    </div>
+  
 }
 
 export default Popular;
