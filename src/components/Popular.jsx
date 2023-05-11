bad0e8ffaf159c4ca1a62432485302b6b8808642
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-05-11 08:53:51 +0700

    move useEffect out of getPopular function and into Popular function. fix api key call

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -2,13 +2,12 @@ import { useEffect } from "react";
 
 function Popular() {
 
-    const getPopular = async () => {
-
-        useEffect(() => {
+    useEffect(() => {
             getPopular();
         }, []);
-        
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.DISHLYST_API_KEY}&number=9`);
+
+    const getPopular = async () => {
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
         const data = await api.json();
         console.log(data);
     }
