52df5d8da23c4b14b5364d3b11f2e108c5b21179
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-05-12 10:09:57 +0700

    save recipes to local storage

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -12,10 +12,21 @@ function Popular() {
             getPopular();
         }, []);
 
+        
+        
     const getPopular = async () => {
+    
+      const check = localStorage.getItem('popular');
+
+      if(check){
+        setPopular(JSON.parse(check));
+      } else{
         const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
         const data = await api.json();
+
+        localStorage.setItem('popular', JSON.stringify(data.recipes));
         setPopular(data.recipes)
+      };
     };
 
   return(
@@ -32,7 +43,7 @@ function Popular() {
             }}>
               {popular.map((recipe) => {
                 return(
-                  <SplideSlide>
+                  <SplideSlide key={recipe.id}>
                     <Card>
                       <p>{recipe.title}</p>
                       <img src={recipe.image} alt={recipe.title} />
