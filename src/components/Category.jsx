731abc5dddaa0f7487195b3cbfd0e41b14b1c845
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-05-12 11:44:49 +0700

    add home and cuisine routes

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -7,22 +7,22 @@ import { NavLink } from 'react-router-dom';
 function Category() {
   return (
     <List>   
-        <NavLink to={'/cuisine/Italian'}>
+        <div>
             <FaPizzaSlice/>
             <h4>Italian</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/American'}>
+        </div>
+        <div>
             <FaHamburger/>
             <h4>American</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/Thai'}>
+        </div>
+        <div>
             <GiNoodles/>
             <h4>Thai</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/Japanese'}>
+        </div>
+        <div>
             <GiChopsticks/>
             <h4>Japanese</h4>
-        </NavLink>
+        </div>
     </List>
 
   )
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,10 +1,16 @@
 import Home from "./Home"
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Cuisine from "./Cuisine";
 
 function Pages() {
   return (
-    <div>
-        <Home/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/cusine" element={<Cuisine/>}/>
+      </Routes>
+    </BrowserRouter>
+ 
   )
 }
 
