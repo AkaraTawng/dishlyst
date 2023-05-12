30bf2cc6d8bc6bc5f74d6ddc3e6e5db5db52ac24
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-05-12 09:54:41 +0700

    move dish title on top of images

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Splide, SlideSlide } from "@splidejs/react-splide"
+import { Splide, SplideSlide } from "@splidejs/react-splide"
 import '@splidejs/splide/dist/css/splide.min.css';
 
 
@@ -20,21 +20,28 @@ function Popular() {
 
   return(
       <div>
-      {popular.map((recipe) => {
-        return(
           <Wrapper>
             <h3>Popular Picks</h3>
-            {popular.map((recipe) => {
-              return(
-                <Card>
-                  <p>{recipe.title}</p>
-                  <img src={recipe.image} alt={recipe.title} />
-                </Card>
-              );
-            })}
+
+            <Splide options={{
+              perPage: 4, 
+              arrows: false, 
+              pagination: false, 
+              drag: 'free', 
+              gap: '5rem'
+            }}>
+              {popular.map((recipe) => {
+                return(
+                  <SplideSlide>
+                    <Card>
+                      <p>{recipe.title}</p>
+                      <img src={recipe.image} alt={recipe.title} />
+                    </Card>
+                  </SplideSlide>
+                );
+              })}
+            </Splide>
           </Wrapper>
-        )
-      })}
     </div>
   )
 }
@@ -47,10 +54,27 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
+  position: relative;
 
   img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
+
+  p {
+    position: absolute;
+    z-index: 2;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+  }
+
 `;
 
 export default Popular;
