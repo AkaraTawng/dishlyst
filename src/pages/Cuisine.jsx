19d7010bbaf9d5ae3991e25d33b4b4145a34d55d
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-05-13 14:40:21 +0700

    add use effect. make getCuisine funciton call in use effect when page loads

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,7 +1,26 @@
+import { motion } from "framer-motion";
+import { styled } from "styled-components";
+import { Link, useParams} from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Cuisine() {
+
+  const [cuisine, setCuisine] = useState([]);
+  
+    const getCuisine = async (name) => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`)
+        const recipes = await data.json();
+        setCuisine(recipes.results);
+    };
+    
+    useEffect(() => {
+      getCuisine('');
+    }, []);
+    
   return (
-    <div>Cuisine</div>
+    <div>
+
+    </div>
   )
 }
 
