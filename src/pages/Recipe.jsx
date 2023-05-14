ecb6422792e370a01fd0f997ca93a40de16ff5a3
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-05-14 09:12:52 +0700

    add activeTab state

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -7,6 +7,7 @@ function Recipe() {
 
   let params = useParams();
   const [details, setDetails] = useState({});
+  const [activeTab, setActiveTab] = useState('instructions');
   const fetchDetails = async () => {
     const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`);
     const detailData = await data.json();
