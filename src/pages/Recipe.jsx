575779b0084ed97c3639e62eb45fbf265e1615d5
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-05-22 09:37:13 +0700

    rearrange order of recipe buttons. fix ingredients api call. change h3s to ps

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -7,7 +7,7 @@ function Recipe() {
 
   let params = useParams();
   const [details, setDetails] = useState({});
-  const [activeTab, setActiveTab] = useState('instructions');
+  const [activeTab, setActiveTab] = useState('summary');
   const fetchDetails = async () => {
     const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`);
     const detailData = await data.json();
@@ -19,7 +19,8 @@ function Recipe() {
     fetchDetails();
   }, [params.name]);
 
-  console.log(details)
+  // console.log(details.extendedIngredients)
+
   return (
     // <DetailWrapper>
     //   <div>
@@ -61,35 +62,35 @@ function Recipe() {
 
       <Info>
         <ButtonContainer>
-          <Button
-            className={activeTab === 'instructions' ? 'active' : ''}
-            onClick={() => setActiveTab('instructions')}>Instructions
-            
+          <Button 
+            className={activeTab === 'summary' ? 'active' : ''}
+            onClick={() => setActiveTab('summary')}>Nutrition Info
           </Button>
           <Button 
             className={activeTab === 'ingredients' ? 'active' : ''}
             onClick={() => setActiveTab('ingredients')}>Ingredients
           </Button>
-          <Button 
-            className={activeTab === 'summary' ? 'active' : ''}
-            onClick={() => setActiveTab('summary')}>Nutrition Info
+          <Button
+            className={activeTab === 'instructions' ? 'active' : ''}
+            onClick={() => setActiveTab('instructions')}>Instructions
           </Button>
         </ButtonContainer>
         {activeTab === 'instructions' && (
           <div>
-            <h3 dangerouslySetInnerHTML={{__html: details.instructions}}></h3>
+            <p dangerouslySetInnerHTML={{__html: details.instructions}}></p>
           </div>
         )}
         {activeTab === 'ingredients' && (
           <ul>
-            {details.extendedIngredients.map((ingredient) => {
-              <li key={ingredient.id}>{ingredient.original}</li>
-            })}
+          {details.extendedIngredients &&
+            details.extendedIngredients.map((ingredient) => (
+              <li key={ingredient.id}> {ingredient.original}</li>
+            ))}
           </ul>
         )}
          {activeTab === 'summary' && (
           <div>
-            <h3 dangerouslySetInnerHTML={{__html: details.summary}}></h3>
+            <p dangerouslySetInnerHTML={{__html: details.summary}}></p>
           </div>
         )}
       </Info>
@@ -117,6 +118,10 @@ const DetailWrapper = styled.div`
     }
     ul {
       margin-top: 2rem;
+    }
+    p {
+      margin-top: 2rem;
+      line-height: 1.5rem;
     }
 
     img {
@@ -147,10 +152,12 @@ const Button = styled.button`
 
 const Info = styled.div`
     /* margin-left: 10rem; */
+    padding: 0 2rem;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    margin-bottom: 2rem;
 `;
 
 const ButtonContainer = styled.div`
