32d5a7ef22eb229fc04ae8de27fc93bc0f38659d
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-05-25 08:58:51 +0700

    import shard styles into cuisine

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,10 +1,7 @@
 import { motion } from "framer-motion";
-import { styled } from "styled-components";
 import { Link, useParams} from "react-router-dom";
 import { useEffect, useState } from "react";
-import { devices } from "../breakpoints";
-// import { GiTransportationRings } from "react-icons/gi";
-
+import { Wrapper, Card, Gradient} from "../SharedStyles.js";
 function Cuisine() {
 
   const [cuisine, setCuisine] = useState([]);
@@ -45,65 +42,6 @@ function Cuisine() {
   )
 }
 
-const Wrapper = styled.div`
-  margin: 4rem 0rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-  align-items: center;
-`;
-
-const Card = styled.div`
-  height: 15rem;
-  width: 15rem;
-  border-radius: 2rem;
-  overflow: hidden;
-  position: relative;
-  margin-bottom: 2rem;
-
-  @media ${devices.tablet} {
-    width: 45%;
-  }
-
-  img {
-    border-radius: 2rem;
-    position: absolute;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-
-  h4 {
-    position: absolute;
-    z-index: 2;
-    left: 50%;
-    bottom: 0%;
-    transform: translate(-50%, 0%);
-    color: white;
-    width: 90%;
-    text-align: center;
-    font-weight: 600;
-    font-size: 1rem;
-    height: 40%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-`;
-
-const Gradient = styled.div`
-  z-index: 1;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.6));
-   &:active
-    {
-    background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.1));
-   }
-`;
 // const Grid = styled(motion.div)`
 //   display: grid;
 //   grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
