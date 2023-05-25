d831e0324edf0f2afe368a5932331a7e5ef2b75a
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-05-25 07:53:59 +0700

    style searched items

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { styled } from "styled-components";
+import { devices } from "../breakpoints";
 
 
 
@@ -21,41 +22,81 @@ useEffect(() => {
 }, [params.search]);
 
   return (
-    <Grid>
+    <Wrapper>
       {searchedRecipes.map((item) => {
         return (
           <Card key={item.id}>
             <Link to={'/recipe/' + item.id}>
               <img src={item.image} alt="" />
               <h4>{item.title}</h4>
+              <Gradient/>
             </Link>
           </Card>
         )
       })}
-    </Grid>
+      </Wrapper>
   )
 };
 
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
-  grid-gap: 3rem;
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
 `;
 
 const Card = styled.div`
-    img {
-      width: 100%;
-      border-radius: 2rem;
-    }
+  height: 15rem;
+  width: 15rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+  margin-bottom: 2rem;
 
-    a {
-      text-decoration: none;
-    }
+  @media ${devices.tablet} {
+    width: 45%;
+  }
 
-    h4 {
-      text-align: center;
-      padding: 1rem;
-    }
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  h4 {
+    position: absolute;
+    z-index: 2;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 90%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `;
+
+const Gradient = styled.div`
+  z-index: 1;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.6));
+   &:active
+    {
+    background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.1));
+   }
+`;
+
 
 export default Searched;
