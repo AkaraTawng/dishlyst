e6080faef47b90eaea83eb64336eb0879bb613e4
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-06-21 06:05:18 +0700

    center align favorites content

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -13,15 +13,13 @@ function Favorites() {
   return (
     <FavPageContainer>
         {favorites.map(favorite => {
-        return <Card>
-              <img src={favorite.image} alt={favorite.title} />
-              <FavLink to={'/recipe/' + favorite.id}>{favorite.title}</FavLink>
-        
-            </Card>
-        
-         
+        return (
+          <Card>
+            <img src={favorite.image} alt={favorite.title}/>
+            <FavLink to={'/recipe/' + favorite.id}>{favorite.title}</FavLink>
+          </Card>
+        )
         })}
-
     </FavPageContainer>
   )
 }
@@ -38,10 +36,29 @@ const Card = styled.div`
   display: flex;
   justify-content: center;
   align-content: center;
+  flex-direction: column;
+  margin-top: 5rem;
+  width: 100%;
+
+  img {
+    height: 10rem;
+    width: 12rem;
+    border-radius: 15px;
+    align-self: center;
+  }
 `;
 
+const SImg = styled.img`
+    height: 10rem;
+    width: 12rem;
+    border-radius: 15px;
+`
+
 const FavLink = styled(Link)`
-  
+  font-weight: 600;
+  font-size: .7rem;
+  text-align: center;
+  padding-top: .5rem;
 `
 
 export default Favorites;
