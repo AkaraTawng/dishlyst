81e43e82a10462cb4ff410a3cf13b81c2483ca7f
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2023-06-28 06:54:46 +0700

    add favorites count element to favorites list button in top right corner

--- src/components/FavoriteBtn.jsx
+++ src/components/FavoriteBtn.jsx
@@ -2,9 +2,13 @@ import { styled } from "styled-components";
 import { NavLink, useLocation} from 'react-router-dom';
 import { BsFillSuitHeartFill } from 'react-icons/bs';
 import { VscChromeClose } from 'react-icons/vsc';
+import { useFavoritesContext } from '../components/FavoritesProvider';
+
 
 function FavoriteBtn() {
-
+  
+  const { favorites } = useFavoritesContext();
+  
   let location = useLocation();
   let pathname = location.pathname;
 
@@ -12,6 +16,9 @@ function FavoriteBtn() {
     <FavLinkContainer>
       {pathname !== '/favorites/' ?  
         <FavLink to={'/favorites/'}>
+           {favorites.length > 0 && <FavoritesCount>
+        {favorites.length}
+      </FavoritesCount>}
           <BsFillSuitHeartFill/>
         </FavLink> 
         : pathname === '/favorites/' ?   
@@ -28,10 +35,23 @@ const FavLinkContainer = styled.div`
     border-radius: 15px;
 `;
 
+const FavoritesCount = styled.div`
+  position: absolute;
+  background-color: red;
+  color: white;
+  border-radius: 50%;
+  padding: 2px 10px;
+  bottom: -9px;
+  left : -5px;
+  font-weight: 700;
+  z-index: 5;
+`;
+
 const FavLink = styled(NavLink)`
 background: linear-gradient(35deg, #494949, #313131);
 border-radius: 15px;
 color: white;
+position: relative;
 padding: 0.8rem 1rem;
 font-size: 1.2rem;
 margin: 10px 10px 0 0;
